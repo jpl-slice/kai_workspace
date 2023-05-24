e62ea37abc71531a595aea8f8a7807daeb0e4bba
Kai Pak <kaipak@jpl.nasa.gov>
2023-05-24 18:33:21 -0500

    Running script for SAR runs.

--- sar_finetune_runs_igarss.sh
+++ sar_finetune_runs_igarss.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-WANDB_PROJECT=SAR_SimCR_finetune_IGARSS_final
-simclr_run=(/work/08452/kaipak/ls6/model_output/SimCLR_SAR/lr_0.15_epochs_250_bs_1026)
+WANDB_PROJECT=SAR_SimCLR_finetune_IGARSS_final
+simclr_run=(/work/08452/kaipak/ls6/model_output/SimCLR_SAR/lr_0.1_epochs_250_bs_1026)
 test_sets=(/home1/08452/kaipak/clover_shared/datasets/California_SAR_Eddies_V2/LocatedEddies_bbupdate20220127/tiles/
            /home1/08452/kaipak/clover_shared/corral/datasets/gee_sar_100k_reference_v1.0.0523/)
+test_files=(/home1/08452/kaipak/clover_shared/datasets/California_SAR_Eddies_V2/LocatedEddies_bbupdate20220127/tiles/pos_centroids_and_bg_tile_labs.csv
+            /home1/08452/kaipak/clover_shared/corral/datasets/gee_sar_100k_reference_v1.0.0523/test_med_subset_linear_labels.csv)
 
 gpu_id=(0 1 2)
 gpu_jobs=${#gpu_id[@]}
 logistic_learning_rates=(0.0001)
-checkpoints=(9, 49, 99, 149, 199, 249)
-debug=True
+checkpoints=(9 49 99 149 199 249)
+# checkpoints=(9 49)
+debug=false
 
 while getopts "p:d:" opt
 do
@@ -18,33 +21,32 @@ do
   esac
 done
 
-for test_set in ${test_sets[@]}; do
+for test_set_idx in {0..1}; do
   for check_point in ${checkpoints[@]}; do
-    echo "Running linear eval on ${simclr_run}, checkpoing {check_point} with ${test_set} on GPU ${gpu}"
     if $debug; then
       echo "CUDA_VISIBLE_DEVICES=${gpu_id[$gpu_jobs - 1]} python ../linear_evaluation/linear_evaluation/finetune.py
-        --config ../linear_evaluation/config/sat_ft_simclrv2.yaml \
-              --test_dir ${dataset} \
-          --pretrain ${simclr_run}/checkpoint_${check_point}.tar \
-              --wandb_project  ${WANDB_PROJECT}\"
+        --config ../linear_evaluation/config/sar_ft_simclrv2.yaml \
+        --test_dir ${test_sets[$test_set_idx]} \
+        --test_file_map ${test_files[$test_set_idx]} \
+        --pretrain ${simclr_run}/checkpoint_${check_point}.tar \
+        --wandb_project ${WANDB_PROJECT}"
     else
-      CUDA_VISIBLE_DEVICES=${gpu_id[$gpu_jobs - 1]} python ../SimCLR_CLOVER/run_ckpt_logistic_pct_loop.py \
-        --config SimCLR_configs/msl_large_bs_config.yaml \
-          --dataset ${dataset} \
-          --dataset_dir ${dataset_dir} \
-          --model_path ${simclr_run} \
-          --use_wandb True \
-              --wandb_project SimCLR_refactor_pretrain_experiments \
-        --use_file_map True \
-              --logistic_learning_rate ${learning_rate} &
+      echo "Starting job on ${gpu_id[$gpu_jobs - 1]}"
+      CUDA_VISIBLE_DEVICES=${gpu_id[$gpu_jobs - 1]} python ../linear_evaluation/linear_evaluation/finetune.py \
+        --config ../linear_evaluation/config/sar_ft_simclrv2.yaml \
+        --test_dir ${test_sets[$test_set_idx]} \
+        --test_file_map ${test_files[$test_set_idx]} \
+        --pretrain ${simclr_run}/checkpoint_${check_point}.tar \
+        --wandb_project ${WANDB_PROJECT} &
     fi
     ((gpu_jobs--))
 
     if [[ $gpu_jobs -eq 0 ]]; then
       wait
-echo "Waiting for previous jobs to complete..."
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-gpu_jobs=${#gpu_id[@]}
-  fi
+      echo "Waiting for previous jobs to complete..."
+      echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      gpu_jobs=${#gpu_id[@]}
+    fi
+  done
 done
 
